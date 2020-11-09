24dd785c5fb57a8df8c159dc77ea5275bfa2fc9b
Peter Arnold <parnold@retrieve.com>
2020-11-09 11:05:14 -0500

    hot reloading via watchgod and  api model defaults

--- app/models/common.py
+++ app/models/common.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from enum import Enum
 
 class ModelName(str, Enum):
@@ -8,7 +8,7 @@ class ModelName(str, Enum):
 
 class GenerateRequest(BaseModel):
       model: ModelName
-      sample_length_in_seconds: int
-      artist: str
-      genre: str
-      lyrics: str
+      sample_length_in_seconds: int = Field(default=20, example=20)
+      artist: str = Field(default="Katy Perry", example="Katy Perry")
+      genre: str = Field(default="Pop", example="Pop")
+      lyrics: str = Field(default="Dueling sails, timelessly quilt the cosmic skies. A tale of reflection and growing, through recursion and glowing. Bound together, woven light forever.", example="Dueling sails, timelessly quilt the cosmic skies. A tale of reflection and growing, through recursion and glowing. Bound together, woven light forever.")
--- docker_run_gpu.sh
+++ docker_run_gpu.sh
@@ -1,5 +1,2 @@
-# Run Worker
-docker run --net=host --gpus all -d entro-jukebox_entro-jukebox:latest
-
-# Run FastAPI
-docker run --net=host --gpus all --entrypoint "/venv/bin/activate && uvicorn app.main:app --host 0.0.0.0 --port 8080 " -d entro-jukebox:latest
+# Run container on GPU
+docker run --net=host --gpus all -v $(pwd)/app:/app -d entro-jukebox_entro-jukebox:latest
